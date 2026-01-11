29c1a02410ab05a236d42330d8bd06178cb94e83
wbj <home.wbj@gmail.com>
2026-01-11 16:52:21 +0800

    取消跟踪一些中间文件

--- imx6ull_lvgl/build/CMakeFiles/lvglTest.dir/lvgl/src/widgets/lv_objx_templ.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lvglTest.dir/lvgl/src/widgets/lv_objx_templ.c.o: \
- /home/wbj/workSpace/linux_mqtt_stm32_car/imx6ull_lvgl/lvgl/src/widgets/lv_objx_templ.c \
- /home/wbj/100ask_imx6ull-sdk/ToolChain/arm-buildroot-linux-gnueabihf_sdk-buildroot/arm-buildroot-linux-gnueabihf/sysroot/usr/include/stdc-predef.h
